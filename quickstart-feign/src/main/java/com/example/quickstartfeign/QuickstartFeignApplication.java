4be0d30c1bd4a373bdf61f44e9bdc240f43db153
zhouqiang <2902511578@qq.com>
2022-07-19 15:04:51 +0800

    :sparkles: Add example with spring cloud openfeign

---
+++ quickstart-feign/src/main/java/com/example/quickstartfeign/QuickstartFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickstartfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstartFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartFeignApplication.class, args);
+	}
+
+}
---
+++ quickstart-feign/src/test/java/com/example/quickstartfeign/QuickstartFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickstartfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
