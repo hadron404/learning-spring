734dbe37cb52a82ddf0f7b0f91f0d46f785000d0
zhouqiang <zhouq@xjjk.com>
2022-07-27 15:13:31 +0800

    :sparkles: Add example with spring-cloud-k8s

---
+++ quickstart-k8s/src/main/java/com/example/quickstartk8s/QuickstartK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickstartk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstartK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartK8sApplication.class, args);
+	}
+
+}
