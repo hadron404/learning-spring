1bd63fc00a92d2dc224140a634e4d47baeb08291
zhouqiang <zhouq@xjjk.com>
2022-09-01 15:55:50 +0800

    :sparkles: 添加一个 redis 的使用实例
    设计一个登录接口,需满足十分钟内连续登录失败5次，30分钟之后才允许再次登录

---
+++ quickstart-redis/src/main/java/com/example/User.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "t_user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Integer gender;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		User user = (User) o;
+		return id != null && Objects.equals(id, user.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
+}
