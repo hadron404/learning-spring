14d2ae9f50603d6bd6e79a507e93336e2e381d12
zhouqiang <zhouq@xjjk.com>
2022-09-02 11:06:03 +0800

    :sparkles: 添加一个 redis 的使用实例
    Spring Boot 集成 redis

--- quickstart-redis/src/test/java/com/example/QuickstartRedisApplicationTests.java
+++ quickstart-redis/src/test/java/com/example/QuickstartRedisApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class QuickstartRedisApplicationTests {
+
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+
+	@Test
+	void integrationRedis_isSuccess() {
+		Assertions.assertDoesNotThrow(() -> {
+			String key = "test";
+			String value = "test value";
+			redisTemplate.opsForValue().set(key, value);
+		});
+	}
+
+	@Test
+	void integrationRedis_store_isSuccess() {
+		String key = "test";
+		String value = "test value";
+		redisTemplate.opsForValue().set(key, value);
+		String storedValue = (String) redisTemplate.opsForValue().get(key);
+		Assertions.assertEquals(value, storedValue);
+	}
 }
